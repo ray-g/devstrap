e342dea618c44a01a4ba0051b6fa20729fd8b673
Ray Guo <ray_g@live.com>
2017-02-11 21:06:26 +0800

    Initialize Install Env

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+echo "Currently only work on Ubuntu destro"
+
+# Update Repo
+sudo apt-get update
+
+# Install ZSH and Oh-My-Zsh
+
+# Install Tmux
+
+# Install EMACS
+
+# Install Docker
+
+# Install VS-Code
+
+# Install Golang
+
+# Install Ruby
+
+# Install Node.JS
+
+# Install Python & PIP
+
+# Install JDK
+
+# Install Maven
+
+# Install Ant
+
+# Install Nginx
