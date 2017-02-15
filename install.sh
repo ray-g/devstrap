2fd0baa88843226bb031e6af2c339b45e5389efa
Ray Guo <ray_g@live.com>
2017-02-16 01:31:23 +0800

    change to execute

--- install.sh
+++ install.sh
@@ -25,54 +25,54 @@ function main_entry() {
     promote_yn "Is this a headless machine?" "headless"
 
     # Update Repo
-    DRYRUN sudo apt-get update
+    execute "sudo apt-get update"
 
     # Install GIT, CURL, WGET
-    DRYRUN sudo apt-get install git curl wget -y
+    execute "sudo apt-get install git curl wget -y"
 
     # Install ZSH and Oh-My-Zsh
-    DRYRUN sudo apt-get install zsh -y
-    DRYRUN curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | DRYRUN sh -
+    execute "sudo apt-get install zsh -y"
+    execute "curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh -"
 
     # Install Tmux
-    DRYRUN sudo apt-get install tmux -y
-    DRYRUN git clone https://github.com/gpakosz/.tmux.git
-    DRYRUN ln -s -f .tmux/.tmux.conf
-    DRYRUN cp .tmux/.tmux.conf.local .
+    execute "sudo apt-get install tmux -y"
+    execute "git clone https://github.com/gpakosz/.tmux.git"
+    execute "ln -s -f .tmux/.tmux.conf"
+    execute "cp .tmux/.tmux.conf.local ."
 
     # Install FZF
-    DRYRUN git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    DRYRUN ~/.fzf/install --all
+    execute "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf"
+    execute "~/.fzf/install --all"
 
     # Install EMACS
     if [ $headless -eq $NO ]; then
-        DRYRUN sudo apt-get install emacs -y
+        execute "sudo apt-get install emacs -y"
     else
-        DRYRUN sudo apt-get install emacs-nox -y
+        execute "sudo apt-get install emacs-nox -y"
     fi
-    DRYRUN git clone https://github.com/seagle0128/.emacs.d
+    execute "git clone https://github.com/seagle0128/.emacs.d"
 
     # Install Python & PIP
-    DRYRUN sudo apt-get install python python-pip -y
+    execute "sudo apt-get install python python-pip -y"
 
     # Install Docker
-    DRYRUN sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-    DRYRUN echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | DRYRUN sudo tee /etc/apt/sources.list.d/docker.list
-    DRYRUN sudo apt-get update
-    DRYRUN apt-cache policy docker-engine
-    DRYRUN sudo apt-get install -y docker-engine
-    DRYRUN sudo usermod -aG docker $(whoami)
-    DRYRUN sudo mkdir /docker
-    DRYRUN sudo ln -s /docker /var/lib/docker
-    DRYRUN sudo echo "\nDOCKER_OPTS=\"-g /docker\"\n" >> /etc/default/docker
-    DRYRUN sudo pip install docker-compose
+    execute "sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D"
+    execute "echo \"deb https://apt.dockerproject.org/repo ubuntu-xenial main\" | sudo tee /etc/apt/sources.list.d/docker.list"
+    execute "sudo apt-get update"
+    execute "apt-cache policy docker-engine"
+    execute "sudo apt-get install -y docker-engine"
+    execute "sudo usermod -aG docker $(whoami)"
+    execute "sudo mkdir /docker"
+    execute "sudo ln -s /docker /var/lib/docker"
+    execute "sudo echo "\nDOCKER_OPTS=\"-g /docker\"\n" >> /etc/default/docker"
+    execute "sudo pip install docker-compose"
 
     # Install VS-Code
     if [ $headless -eq $NO ]; then
         local vscode="vscode_stable_myenv.deb"
-        DRYRUN wget https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -O $vscode
-        DRYRUN sudo dpkg -i $vscode
-        DRYRUN rm $vscode
+        execute "wget https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -O $vscode"
+        execute "sudo dpkg -i $vscode"
+        execute "rm $vscode"
     fi
 
     # Install Golang
@@ -80,18 +80,18 @@ function main_entry() {
     local GO_VER="1.7.5"
     local GO_OS="linux"
     local GO_ARCH="amd64"
-    DRYRUN wget https://storage.googleapis.com/golang/go${GO_VER}.${GO_OS}-${GO_ARCH}.tar.gz
-    DRYRUN tar -C /usr/local -xzf go${GO_VER}.${GO_OS}-${GO_ARCH}.tar.gz
+    execute "wget https://storage.googleapis.com/golang/go${GO_VER}.${GO_OS}-${GO_ARCH}.tar.gz"
+    execute "tar -C /usr/local -xzf go${GO_VER}.${GO_OS}-${GO_ARCH}.tar.gz"
 
     # Install Ruby
-    DRYRUN apt-get install ruby2.3 ruby2.3-dev -y
-    DRYRUN sudo gem install rubocop
+    execute "apt-get install ruby2.3 ruby2.3-dev -y"
+    execute "sudo gem install rubocop"
 
     # Install Node.JS
     # https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
-    DRYRUN curl -sL https://deb.nodesource.com/setup_6.x | DRYRUN sudo -E bash -
-    DRYRUN sudo apt-get install -y nodejs
-    DRYRUN npm install -g gulp-cli
+    execute "curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -"
+    execute "sudo apt-get install -y nodejs"
+    execute "npm install -g gulp-cli"
 
     # Install JDK
 
@@ -100,7 +100,7 @@ function main_entry() {
     # Install Ant
 
     # Install Nginx
-    DRYRUN sudo apt-get install -y nginx
+    execute "sudo apt-get install -y nginx"
 
     if [ ! -z "$CURRENT_DIR" ]; then
         cd $CURRENT_DIR
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -93,14 +93,6 @@ function DEBUG_END() {
     DEBUG set +x
 }
 
-function DRYRUN() {
-    if [ "$_DRYRUN" == "on" ]; then
-        echo "DRYRUN: $@"
-    else
-        $@
-    fi
-}
-
 function execute() {
     local -r CMDS="$1"
     local -r MSG="${2:-$1}"
@@ -109,51 +101,38 @@ function execute() {
     local exitCode=0
     local cmdsPID=""
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    if [ "$_DRYRUN" == "on" ]; then
+        echo "DRYRUN: $CMDS"
+    else
+        # If the current process is ended,
+        # also end all its subprocesses.
+        set_trap "EXIT" "kill_all_subprocesses"
 
-    # If the current process is ended,
-    # also end all its subprocesses.
-
-    set_trap "EXIT" "kill_all_subprocesses"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Execute commands in background
-
-    eval "$CMDS" \
+        # Execute commands in background
+        eval "$CMDS" \
         &> /dev/null \
         2> "$TMP_FILE" &
 
-    cmdsPID=$!
+        cmdsPID=$!
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        # Show a spinner if the commands
+        # require more time to complete.
+        show_spinner "$cmdsPID" "$CMDS" "$MSG"
 
-    # Show a spinner if the commands
-    # require more time to complete.
+        # Wait for the commands to no longer be executing
+        # in the background, and then get their exit code.
+        wait "$cmdsPID" &> /dev/null
+        exitCode=$?
 
-    show_spinner "$cmdsPID" "$CMDS" "$MSG"
+        # Print output based on what happened.
+        print_result $exitCode "$MSG"
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        if [ $exitCode -ne 0 ]; then
+            print_error_stream < "$TMP_FILE"
+        fi
 
-    # Wait for the commands to no longer be executing
-    # in the background, and then get their exit code.
-
-    wait "$cmdsPID" &> /dev/null
-    exitCode=$?
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Print output based on what happened.
-
-    print_result $exitCode "$MSG"
-
-    if [ $exitCode -ne 0 ]; then
-        print_error_stream < "$TMP_FILE"
+        rm -rf "$TMP_FILE"
     fi
-
-    rm -rf "$TMP_FILE"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     return $exitCode
 }
@@ -180,8 +159,6 @@ function get_os() {
 function get_os_version() {
     local os=""
     local version=""
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     os="$(get_os)"
 
