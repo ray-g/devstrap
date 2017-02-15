529b944f141399b9fa6db9fd92c26531d28f2dfe
Ray Guo <ray_g@live.com>
2017-02-15 17:11:13 +0800

    add functions to detect os

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -100,3 +100,96 @@ function DRYRUN() {
         $@
     fi
 }
+
+function execute() {
+    local -r CMDS="$1"
+    local -r MSG="${2:-$1}"
+    local -r TMP_FILE="$(mktemp /tmp/XXXXXX)"
+
+    local exitCode=0
+    local cmdsPID=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # If the current process is ended,
+    # also end all its subprocesses.
+
+    set_trap "EXIT" "kill_all_subprocesses"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Execute commands in background
+
+    eval "$CMDS" \
+        &> /dev/null \
+        2> "$TMP_FILE" &
+
+    cmdsPID=$!
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Show a spinner if the commands
+    # require more time to complete.
+
+    show_spinner "$cmdsPID" "$CMDS" "$MSG"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Wait for the commands to no longer be executing
+    # in the background, and then get their exit code.
+
+    wait "$cmdsPID" &> /dev/null
+    exitCode=$?
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Print output based on what happened.
+
+    print_result $exitCode "$MSG"
+
+    if [ $exitCode -ne 0 ]; then
+        print_error_stream < "$TMP_FILE"
+    fi
+
+    rm -rf "$TMP_FILE"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    return $exitCode
+}
+
+function get_os() {
+    local os=""
+    local kernelName=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    kernelName="$(uname -s)"
+
+    if [ "$kernelName" == "Darwin" ]; then
+        os="macos"
+    elif [ "$kernelName" == "Linux" ] && [ -e "/etc/lsb-release" ]; then
+        os="ubuntu"
+    else
+        os="$kernelName"
+    fi
+
+    printf "%s" "$os"
+}
+
+function get_os_version() {
+    local os=""
+    local version=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    os="$(get_os)"
+
+    if [ "$os" == "macos" ]; then
+        version="$(sw_vers -productVersion)"
+    elif [ "$os" == "ubuntu" ]; then
+        version="$(lsb_release -d | cut -f2 | cut -d' ' -f2)"
+    fi
+
+    printf "%s" "$version"
+}
