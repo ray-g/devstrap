8bff5552266d7acbc672feb8f297669350a917e8
Ray Guo <ray_g@live.com>
2017-02-19 17:17:32 +0800

    add go utils

---
+++ scripts/install/common/go.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Go utilities
+go get -u -v github.com/nsf/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v github.com/zmb3/gogetdoc
+go get -u -v github.com/golang/lint/golint
+go get -u -v github.com/lukehoban/go-outline
+go get -u -v sourcegraph.com/sqs/goreturns
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v github.com/tpng/gopkgs
+go get -u -v github.com/newhook/go-symbols
+go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v github.com/cweill/gotests/...
