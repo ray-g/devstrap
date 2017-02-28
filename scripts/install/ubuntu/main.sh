03e2000cc45ae1d0dced779b381f68c53420e1ed
Ray Guo <ray_g@live.com>
2017-02-28 23:42:36 +0800

    fail and exit in install functions

--- scripts/install/ubuntu/main.sh
+++ scripts/install/ubuntu/main.sh
@@ -13,15 +13,15 @@ function post_install() {
 }
 
 function install_docker() {
-    execute "sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D"
-    execute "echo \"deb https://apt.dockerproject.org/repo ubuntu-xenial main\" | sudo tee /etc/apt/sources.list.d/docker.list"
-    update
-    execute "apt-cache policy docker-engine"
-    install_package "docker-engine" "Docker Engine"
-    execute "sudo usermod -aG docker $(whoami)"
-    execute "sudo mkdir /docker"
-    execute "sudo ln -s /docker /var/lib/docker"
-    execute "sudo echo \"DOCKER_OPTS=\\\"-g /docker\\\"\" >> /etc/default/docker"
+    execute "sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D" || return $?
+    execute "echo \"deb https://apt.dockerproject.org/repo ubuntu-xenial main\" | sudo tee /etc/apt/sources.list.d/docker.list" || return $?
+    update || return $?
+    execute "apt-cache policy docker-engine" || return $?
+    install_package "docker-engine" "Docker Engine" || return $?
+    execute "sudo usermod -aG docker $(whoami)" || return $?
+    execute "sudo mkdir /docker" || return $?
+    execute "sudo ln -s /docker /var/lib/docker" || return $?
+    execute "sudo echo \"DOCKER_OPTS=\\\"-g /docker\\\"\" >> /etc/default/docker" || return $?
 
     # Install Docker Compose
     if ! cmd_exists "docker-compose"; then
@@ -32,7 +32,7 @@ function install_docker() {
 function install_nodejs() {
     # Install Node.JS
     # https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
-    execute "curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -"
+    execute "curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -" || return $?
     install_package "nodejs" "NodeJS"
 }
 
@@ -40,15 +40,17 @@ function install_golang() {
     local GO_VER="1.7.5"
     local GO_OS="linux"
     local GO_ARCH="amd64"
-    execute "wget https://storage.googleapis.com/golang/go${GO_VER}.${GO_OS}-${GO_ARCH}.tar.gz"
+    execute "wget https://storage.googleapis.com/golang/go${GO_VER}.${GO_OS}-${GO_ARCH}.tar.gz" || return $?
     execute "sudo tar -C /usr/local -xzf go${GO_VER}.${GO_OS}-${GO_ARCH}.tar.gz"
 }
 
 function install_vscode() {
     local vscode="vscode_stable_myenv.deb"
-    execute "wget https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -O $vscode"
+    execute "wget https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -O $vscode" || return $?
     execute "sudo dpkg -i $vscode"
+    local exitCode=$?
     execute "rm $vscode"
+    return $exitCode
 }
 
 # Register apt installer
