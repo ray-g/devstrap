704a51f890707892977eb0e457cda6c3d676fd9d
Ray Guo <ray_g@live.com>
2017-03-02 01:07:18 +0800

    add mark for testing env

--- test/ubuntu.16.04.docker.setup.sh
+++ test/ubuntu.16.04.docker.setup.sh
@@ -1,3 +1,6 @@
 sed -i 's/archive.ubuntu.com/cn.archive.ubuntu.com/g' /etc/apt/sources.list
 apt-get update && apt-get install -y git sudo whiptail apt-transport-https
 useradd -s /bin/bash -m -U admin
+#/usr/sbin/useradd -p \`openssl passwd -1 $PASS\` $USER
+# sudo visudo
+# Defaults        env_reset,timestamp_timeout=30
