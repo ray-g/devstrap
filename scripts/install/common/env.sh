f9946f5ef5f2a491e7cb3f5ffd86e3fa6c6a2ef9
Ray Guo <ray_g@live.com>
2017-03-02 01:05:19 +0800

    change back to execute

--- scripts/install/common/env.sh
+++ scripts/install/common/env.sh
@@ -3,11 +3,11 @@
 function install_omz() {
     # Install Oh-My-ZSH
     # http://ohmyz.sh/
-    # Not able to 'execute' due to quotes...
-    # execute "sh -c \"$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)\"" "Oh-My-ZSH"
+    local exitCode=0
     if [ ! -e ~/.oh-my-zsh ]; then
-        sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's/env zsh/ /g')" > /dev/null
-        local exitCode=$?
+        # TODO: sudo chsh -s $(grep /zsh$ /etc/shells | tail -1) $(whoami)
+        execute "sh -c \"\$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's/env zsh/ /g')\"" "Oh-My-ZSH"
+        exitCode=$?
     fi
     execute "sync_repo https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
     execute "sync_repo https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
