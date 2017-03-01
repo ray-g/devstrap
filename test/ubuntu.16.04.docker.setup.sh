7d2f9fe7200714d7f598a3f9a70f952534afd28c
Ray Guo <ray_g@live.com>
2017-03-01 22:22:28 +0800

    update setup testing env

--- test/ubuntu.16.04.docker.setup.sh
+++ test/ubuntu.16.04.docker.setup.sh
@@ -1,2 +1,3 @@
 sed -i 's/archive.ubuntu.com/cn.archive.ubuntu.com/g' /etc/apt/sources.list
 apt-get update && apt-get install -y git sudo whiptail apt-transport-https
+useradd -s /bin/bash -m -U admin
