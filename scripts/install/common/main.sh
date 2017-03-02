f75664005ac7ff90bad5ccd65d40e73c7dc6db01
Ray Guo <ray_g@live.com>
2017-03-02 14:27:21 +0800

    update calibre installer

--- scripts/install/common/main.sh
+++ scripts/install/common/main.sh
@@ -6,3 +6,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
     && . "gem.sh" \
     && . "env.sh" \
     && cd - &> /dev/null
+
+function install_calibre() {
+    execute "sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.py | sudo python -c \"import sys; main=lambda:sys.stderr.write('Download failed\\n'); exec(sys.stdin.read()); main()\"" "Calibre"
+}
