ffa8359bdd4a0c3238d546f13da0dd42963a29e2
Ray Guo <ray_g@live.com>
2017-03-04 00:58:06 +0800

    update check for result

--- test/check_travis_result.sh
+++ test/check_travis_result.sh
@@ -1,16 +1,37 @@
 #!/usr/bin/env bash
 
-echo 'cat /etc/passwd:'
-cat /etc/passwd
+BASE_DIR="$(dirname ${BASH_SOURCE%/*})"
 
-echo 'ls -al ~:'
+. "${BASE_DIR}/scripts/utils.sh"
+
+function print_space_lines() {
+    echo
+    echo
+}
+
+print_info "cat /etc/passwd:"
+cat /etc/passwd | grep $(whoami)
+cat /etc/passwd | grep $(whoami) | grep "zsh"
+print_result $? "Oh-My-Zsh change shell"
+
+print_space_lines
+
+print_info "ls -al ~:"
 ls -al ~
 
-echo 'ls -al ~/bin'
+print_space_lines
+
+print_info "ls -la ~/.oh-my-zsh"
+ls -al ~/.oh-my-zsh
+
+print_space_lines
+
+print_info "ls -al ~/bin"
 ls -al ~/bin
 
-echo 'ls -al $PWD'
-echo "PWD: $PWD"
+print_space_lines
+
+print_info "ls -al $PWD"
 ls -al $PWD
 
 exit 0
