8ec9d396e5debbcd630aa6243068e93c35a5c73a
Ray Guo <ray_g@live.com>
2017-03-04 02:44:50 +0800

    Log shell before install on Travis

--- test/test_prerequisites.sh
+++ test/test_prerequisites.sh
@@ -5,3 +5,6 @@ BASE_DIR="$(dirname ${BASH_SOURCE%/*})"
 . "${BASE_DIR}/scripts/utils.sh"
 
 execute "check_and_install_whiptail"
+
+# Log shell before install on Travis
+cat /etc/passwd | grep $(whoami)
