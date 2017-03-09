7aa397c5978eb76f407135791c3db0a6a01e8987
Ray Guo <ray_g@live.com>
2017-03-09 21:16:57 +0800

    try upgrade osx bash version

--- test/test_prerequisites.sh
+++ test/test_prerequisites.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    if ! cmd_exists 'brew'; then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    brew install bash bash-completion
+    sudo sh -c "echo '$(brew --prefix)/bin/bash' >> /etc/shells"
+    sudo chsh -s $(brew --prefix)/bin/bash $(whoami)
+fi
+
 BASE_DIR="$(dirname ${BASH_SOURCE%/*})"
 
 . "${BASE_DIR}/scripts/utils.sh"
