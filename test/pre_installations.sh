e2b53826c438f2433db1dc3f83a9666093e9ecad
Ray Guo <ray_g@live.com>
2017-03-09 22:45:08 +0800

    add pre installations on travis

---
+++ test/pre_installations.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    if ! hash brew 2>/dev/null; then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    brew install bash bash-completion
+    sudo sh -c "echo '$(brew --prefix)/bin/bash' >> /etc/shells"
+    sudo chsh -s $(brew --prefix)/bin/bash $(whoami)
+fi
--- test/test_prerequisites.sh
+++ test/test_prerequisites.sh
@@ -1,21 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    if ! hash brew 2>/dev/null; then
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
-    brew install bash bash-completion
-    sudo sh -c "echo '$(brew --prefix)/bin/bash' >> /etc/shells"
-    sudo chsh -s $(brew --prefix)/bin/bash $(whoami)
-else
+BASE_DIR="$(dirname ${BASH_SOURCE%/*})"
 
-    BASE_DIR="$(dirname ${BASH_SOURCE%/*})"
+. "${BASE_DIR}/scripts/utils.sh"
 
-    . "${BASE_DIR}/scripts/utils.sh"
+execute "check_and_install_whiptail"
 
-    execute "check_and_install_whiptail"
-
-    # Log shell before install on Travis
-    cat /etc/passwd | grep $(whoami)
-
-fi
+# Log shell before install on Travis
+cat /etc/passwd | grep $(whoami)
