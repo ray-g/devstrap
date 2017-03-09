5f2012dc7df829d759288713efa29225ff0cce1c
Ray Guo <ray_g@live.com>
2017-03-09 21:50:08 +0800

    try fix travis osx...

--- test/test_prerequisites.sh
+++ test/test_prerequisites.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    if ! cmd_exists 'brew'; then
+    if ! hash brew 2>/dev/null; then
         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
     brew install bash bash-completion
     sudo sh -c "echo '$(brew --prefix)/bin/bash' >> /etc/shells"
     sudo chsh -s $(brew --prefix)/bin/bash $(whoami)
+else
+
+    BASE_DIR="$(dirname ${BASH_SOURCE%/*})"
+
+    . "${BASE_DIR}/scripts/utils.sh"
+
+    execute "check_and_install_whiptail"
+
+    # Log shell before install on Travis
+    cat /etc/passwd | grep $(whoami)
+
 fi
-
-BASE_DIR="$(dirname ${BASH_SOURCE%/*})"
-
-. "${BASE_DIR}/scripts/utils.sh"
-
-execute "check_and_install_whiptail"
-
-# Log shell before install on Travis
-cat /etc/passwd | grep $(whoami)
