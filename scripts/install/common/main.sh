d4784e430c02214c7e129bac45b694debccdc1ba
Ray Guo <ray_g@live.com>
2018-09-02 09:41:08 +0800

    Add cheat.sh

--- scripts/install/common/main.sh
+++ scripts/install/common/main.sh
@@ -10,3 +10,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 function install_calibre() {
     execute "sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.py | sudo python -c \"import sys; main=lambda:sys.stderr.write('Download failed\\n'); exec(sys.stdin.read()); main()\"" "Calibre"
 }
+
+function install_cheat_sh() {
+    execute "curl https://cht.sh/:cht.sh > ~/bin/cht.sh && chmod +x ~/bin/cht.sh"
+}
