c4d0186e3169f96ee80e65be78377e21c40d73d9
Ray Guo <ray_g@live.com>
2019-11-08 16:42:35 +0800

    update software versions

--- scripts/install/versions.sh
+++ scripts/install/versions.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # https://golang.org/dl/
-DEVSTRAP_GO_VER="1.13"
+DEVSTRAP_GO_VER="1.13.4"
 
 # https://github.com/BurntSushi/ripgrep/releases
-DEVSTRAP_RG_VER="0.10.0"
+DEVSTRAP_RG_VER="11.0.2"
 
 # https://github.com/sharkdp/fd/releases
-DEVSTRAP_FD_VER="7.1.0"
+DEVSTRAP_FD_VER="7.4.0"
 
 # https://github.com/peco/peco/releases
 DEVSTRAP_PECO_VER="0.5.3"
 
 # https://github.com/google/protobuf/releases
-DEVSTRAP_PROTOBUF_VER="3.9.1"
+DEVSTRAP_PROTOBUF_VER="3.10.1"
